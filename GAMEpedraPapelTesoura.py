471d8573489001b04fa6164bb9daf637e8956c0c
João Henrique Guimarães <81715625+younglua-wv@users.noreply.github.com>
2021-09-23 02:41:02 +0000

    alteração no GAME Jokenpo

--- GAMEpedraPapelTesoura.py
+++ GAMEpedraPapelTesoura.py
@@ -1,46 +1,47 @@
 from random import randint
 from time import sleep
-
-opcao = int(input("Suas opções:\n[ 0 ] PEDRA\n[ 1 ] PAPEL\n[ 2 ] TESOURA\n\nQual é a sua jogada? "))
+nome = input("Nome do Jogador: ")
+itens = ("Pedra", "Papel", "Tesoura")
+opcao = int(input("\nSuas opções:\n\n[ 0 ] PEDRA\n[ 1 ] PAPEL\n[ 2 ] TESOURA\n\nQual é a sua jogada? "))
 
 sleep(1)
-print("JO")
+print("\nJO")
 sleep(1)
 print("KEN")
 sleep(1)
-print("PO!!!")
+print("PO!!!\n")
 sleep(2)
 
 jogadaMaquina = randint(0, 2)
 
 print("-="*20)
-print(f"COMPUTADOR jogou {jogadaMaquina}\nJOGADOR jogou {opcao}")
+print(f"COMPUTADOR escolheu {itens[jogadaMaquina]}")
+print(f"{nome} escolheu {itens[opcao]}")
 print("-="*20)
 
 if jogadaMaquina == 0:
   if opcao == 0:
     print("EMPATE")
   elif opcao == 1:
-    print("JOGADOR VENCE")
+    print(f"{nome} VENCEU")
   else:
-    print("COMPUTADOR VENCE")
+    print("COMPUTADOR VENCEU")
 
 elif jogadaMaquina == 1:
   if opcao == 0:
-    print("COMPUTADOR VENCE")
+    print("COMPUTADOR VENCEU")
   elif opcao == 1:
     print("EMPATE")
   else:
-    print("JOGADOR VENCE")
+    print(f"{nome} VENCEU")
 
 elif jogadaMaquina == 2:
   if opcao == 0:
-    print("JOGADOR VENCE")
+    print(f"{nome} VENCEU")
   elif opcao == 1:
-    print("COMPUTADOR VENCE")
+    print("COMPUTADOR VENCEU")
   else:
     print("EMPATE")
 
 else:
   print("OPÇÃO INVÁLIDA!")
-
