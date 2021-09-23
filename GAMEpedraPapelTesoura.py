5b492f349408ddcaa8459e9ab61c36affdd15778
João Henrique Guimarães <81715625+younglua-wv@users.noreply.github.com>
2021-09-23 03:58:19 +0000

    Atualizando GamePedraPapelTesoura.py

--- GAMEpedraPapelTesoura.py
+++ GAMEpedraPapelTesoura.py
@@ -1,16 +1,19 @@
 from random import randint
 from time import sleep
+
 nome = input("Nome do Jogador: ")
+
 itens = ("Pedra", "Papel", "Tesoura")
 
 opcao = int(input("\nSuas opções:\n\n[ 0 ] PEDRA\n[ 1 ] PAPEL\n[ 2 ] TESOURA\n\nQual é a sua jogada? "))
 
 jogadaMaquina = randint(0, 2)
 
-if 0 < jogadaMaquina > 2 or 0 < opcao > 2:
+if opcao > 2 or opcao < 0:
   print("\nOpção inválida!\nTente novamente!")
 
 else:
+  
   sleep(1)
   print("\nJO")
   sleep(1)
@@ -20,8 +23,11 @@ else:
   sleep(2)
 
   print("-="*20)
+  
   print(f"COMPUTADOR escolheu {itens[jogadaMaquina]}")
+  
   print(f"{nome} escolheu {itens[opcao]}")
+  
   print("-="*20)
 
   if jogadaMaquina == 0:
